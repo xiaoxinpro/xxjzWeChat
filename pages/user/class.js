b791f0dc4ba2bb4df92aa56473d0e12d88b25358
Chishin <pro@xxgzs.org>
2017-11-15 23:02:44 +0800

    构建分类管理页面框架
    搭建支出分类管理界面框架

--- pages/user/class.js
+++ pages/user/class.js
@@ -1,66 +1,115 @@
 // class.js
+var that = this;
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    tabs: ["支出分类", "收入分类"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0
+  },
+
+  /**
+   * 点击标签事件
+   */
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  },
+
+  /**
+   * 添加支出分类
+   */
+  submit_outClass: function(e){
+    var className = e.detail.value.class_name;
+    console.log("添加支出分类：",e.detail.value);
+  },
+
+  /**
+   * 编辑支出分类
+   */
+  btnOutClassEdit: function(e){
+    var classId = e.target.dataset.id;
+    console.log("编辑支出分类：", e.target.dataset);
+  },
+
+  /**
+   * 转移支出分类
+   */
+  btnOutClassChange: function (e) {
+    var classId = e.target.dataset.id;
+    console.log("转移支出分类：", e.target.dataset);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    var frmType = options.type ? options.type : 0;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * frmType,
+          activeIndex: frmType
+        });
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
