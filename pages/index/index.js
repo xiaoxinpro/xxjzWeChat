ea47933ba5f50af4b8c2b48a03441e87f4a2735f
Chishin <pro@xxgzs.org>
2018-01-28 14:58:32 +0800

    增加启动页面微信登陆
    优化启动流程

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,13 @@
 //index.js
 //获取应用实例
 var app = getApp();
+var code = "";
 Page({
   data: {
     motto: '小歆记账',
     login: '绑定已有帐号',
     regist: '新建帐号',
+    lock: true,
     userInfo: {}
   },
   //事件处理函数
@@ -16,16 +18,19 @@ Page({
   },
   bindViewLogin: function() {
     wx.navigateTo({
-      url: '../login/login',
+      url: '../login/login?code=' + code,
     })
   },
   bindViewRegist: function() {
-    wx.showModal({
-      title: '暂不支持注册',
-      content: '微信小程序暂不支持小歆记账新用户注册，请到小歆记账官网注册后再来登陆！',
-      showCancel: false,
-      confirmText: '知道了'
+    wx.navigateTo({
+      url: '../login/regist?code=' + code,
     })
+    // wx.showModal({
+    //   title: '暂不支持注册',
+    //   content: '微信小程序暂不支持小歆记账新用户注册，请到小歆记账官网注册后再来登陆！',
+    //   showCancel: false,
+    //   confirmText: '知道了'
+    // })
   },
   onLoad: function () {
     console.log('加载小程序，检测URL：', getApp().URL);
@@ -34,14 +39,17 @@ Page({
     // 清除PHPSESSID
     wx.removeStorageSync('PHPSESSID');
 
+    // 尝试登陆
+    Login(that);
+
     //获取登陆用户数据
-    let user = wx.getStorageSync('user');
-    if (user && user.uid > 0) {
-      wx.navigateTo({
-        url: '../login/login?username=' + user.username + '&password=' + user.password,
-      });
-      return;
-    }
+    // let user = wx.getStorageSync('user');
+    // if (user && user.uid > 0) {
+    //   wx.navigateTo({
+    //     url: '../login/login?username=' + user.username + '&password=' + user.password,
+    //   });
+    //   return;
+    // }
 
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
@@ -77,3 +85,72 @@ Page({
     })
   }
 })
+
+function LoadDone(that) {
+  wx.hideLoading();
+  that.setData({
+    lock: false
+  });
+}
+
+function Login(that) {
+  // 尝试登陆
+  wx.showLoading({
+    title: '加载中',
+  });
+  wx.login({
+    success: function (res) {
+      if (res.code) {
+        code = res.code;
+        console.log('获取用户登录态成功：' + res.code)
+        wx.request({
+          url: getApp().URL + '/xxjzApp/index.php?s=/Home/Login/login_weixin',
+          data: {
+            js_code: res.code
+          },
+          method: 'GET',
+          success: function (res) {
+            LoadDone(that);
+            console.log('提交登陆完成：', res.data);
+            //保存sessionid
+            if (res.data.hasOwnProperty('sessionid')) {
+              wx.setStorageSync('PHPSESSID', res.data.sessionid);
+            }
+            //处理登陆结果
+            var uid = parseInt(res.data.uid);
+            var uname = res.data.uname;
+            if (uid == 0) {
+              wx.showModal({
+                title: '登陆失败',
+                content: uname,
+              });
+            } else if (uid > 0) {
+              wx.showToast({
+                title: '登陆成功',
+              });
+              wx.setStorage({
+                key: 'user',
+                data: {
+                  uid: uid,
+                  username: uname,
+                },
+                success: function(){
+                  setTimeout(function () {
+                    //跳转到用户主页
+                    wx.reLaunch({ url: "../main/main?uid=" + uid + "&uname=" + uname });
+                  }, 500);
+                }
+              });
+            }
+          }
+        });
+      } else {
+        LoadDone(that);
+        console.log('获取用户登录态失败！' + res.errMsg)
+      }
+    },
+    fail: function () {
+      LoadDone(that);
+    }
+  });
+}
