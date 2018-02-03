f4942bcbbad5e26e1a957f1f5125b74fe7b43d0f
Chishin <pro@xxgzs.org>
2018-02-03 21:45:27 +0800

    增加获取分类公共函数
    重构推出登陆公共函数

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //app.js
 App({
   //全局变量
-  // URL: 'https://ide.xxgzs.org',
-  URL: 'https://jz.xxgzs.org',
+  URL: 'https://ide.xxgzs.org',
+  // URL: 'https://jz.xxgzs.org',
 
   onLaunch: function () {
 
@@ -51,9 +51,54 @@ App({
     })
   },
 
-  Logout: function() {
-    wx.removeStorageSync('user');
-    wx.reLaunch({ url: "/pages/index/index" });
+  // 获取分类数据
+  GetClassData: function(uid, callback) {
+    console.log('uid:',uid)
+    if(uid > 0) {
+      var session_id = wx.getStorageSync('PHPSESSID');//本地取存储的sessionID  
+      if (session_id != "" && session_id != null) {
+        var header = { 'content-type': 'application/x-www-form-urlencoded', 'Cookie': 'PHPSESSID=' + session_id }
+      } else {
+        callback(false, 0, "内存数据出错，请登陆后再试。");
+      } 
+      wx.request({
+        url: getApp().URL + '/xxjzApp/index.php?s=/Home/Api/aclass',
+        data: { type: 'get' },
+        header: header,
+        success: function (res) {
+          console.log('获取分类数据:', res);
+          if (res.hasOwnProperty('data')) {
+            let ret = res['data'];
+            if (ret['uid'] == uid) {
+              let data = ret['data'];
+              wx.setStorage({
+                key: 'inClass',
+                data: data.in
+              });
+              wx.setStorage({
+                key: 'outClass',
+                data: data.out
+              });
+              wx.setStorage({
+                key: 'allClass',
+                data: data.all
+              });
+              callback(true, Object.getOwnPropertyNames(data.all).length, data);
+            } else {
+              callback(false, 0, "登录验证已过期，请重新登录。");
+            }
+          }
+        }
+      });
+    } else {
+      callback(false, 0, "用户登陆超时，请重新登陆。");
+    }
+  },
+
+  // 退出登陆
+  Logout: function(callback) {
+    wx.clearStorage();
+    callback({ url: "/pages/index/index" });
   },
 
   globalData:{
