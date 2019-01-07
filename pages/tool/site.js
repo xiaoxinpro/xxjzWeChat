836cc0932b861249bf61d634c1231661df0c6cf0
Chishin <pro@xxgzs.org>
2019-01-07 20:11:59 +0800

    增加工具栏设置页面的返回按钮

--- pages/tool/site.js
+++ pages/tool/site.js
@@ -66,7 +66,7 @@ Page({
   },
 
   /**
-   * 
+   * 保存按钮事件
    */
   bindSubmit: function(){
     var ret = {
@@ -86,6 +86,11 @@ Page({
         },300);
       }
     })
+  },
+
+  /** 返回按钮事件 */
+  bindBack: function () {
+    wx.navigateBack();
   },
 
   /**
