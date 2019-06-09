02083a2bca88321aa8674b2a16aa2ecaeb7cf358
Chishin <pro@xxgzs.org>
2019-06-09 11:47:57 +0800

    优化头像质量，删除冗余代码

--- pages/user/user.js
+++ pages/user/user.js
@@ -28,23 +28,12 @@ Page({
     var that = this;
     wx.chooseImage({
       count: 1, // 最多可以选择的图片张数，默认9
-      sizeType: ['compressed'], // original 原图，compressed 压缩图，默认二者都有
+      sizeType: ['original'], // original 原图，compressed 压缩图，默认二者都有
       sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
       success: function (res) {
         wx.redirectTo({
           url: `./site/avatar?src=${res.tempFilePaths[0]}`
-        })
-        // wx.saveFile({
-        //   tempFilePath: res.tempFilePaths[0],
-        //   success: function (res) {
-        //     var avatarPath = res.savedFilePath;
-        //     that.setData({ avatarPath: avatarPath });
-        //     wx.setStorage({
-        //       key: 'avatarPath',
-        //       data: avatarPath,
-        //     })
-        //   }
-        // });
+        });
       },
       complete: function (res) {
         console.log(res);
