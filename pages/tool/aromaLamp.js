c7c5e687eca72bf4af2424b1f428c8e0047bbcb7
Chishin <pro@xxgzs.org>
2019-07-05 22:54:59 +0800

    实现色温选择控制

--- pages/tool/aromaLamp.js
+++ pages/tool/aromaLamp.js
@@ -6,61 +6,86 @@ Page({
    */
   data: {
 
+    colorItems: [
+      {name: '冷光',value: '0',checked: true},
+      {name: '暖光',value: '1'},
+      {name: '冷暖光',value: '3'}
+    ],
+
+  },
+
+  /**
+   * 色温选择事件
+   */
+  colorChange: function(e) {
+    var colorItems = this.data.colorItems;
+    for (var i in colorItems) {
+      colorItems[i].checked = colorItems[i].value == e.detail.value;
+      if (colorItems[i].checked) {
+        //获取选择色温
+      }
+    }
+    // 更新UI
+    this.setData({
+      colorItems: colorItems,
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    wx.setNavigationBarTitle({ title: '香薰灯'});
+  onLoad: function(options) {
+    wx.setNavigationBarTitle({
+      title: '香薰灯'
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
