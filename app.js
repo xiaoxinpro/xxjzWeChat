35afe3654be5fed64caddaa1fd454582d4e8331e
Chishin <pro@xxgzs.org>
2019-07-31 14:59:20 +0800

    增加获取窗口和屏幕高度的属性

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ App({
   ServerVersion: '0.0.1',
 
   onLaunch: function() {
+    wx.getSystemInfo({
+      success: res => {
+        this.globalData.systemInfo = res
+        this.globalData.windowHeight = res.windowHeight / (res.windowWidth / 750)
+        this.globalData.screenHeight = res.screenHeight / (res.screenWidth / 750)
+      }
+    });
     this.getVersion();
   },
 
@@ -426,6 +433,9 @@ App({
   },
 
   globalData: {
-    userInfo: null
+    userInfo: null,
+    systemInfo: null,
+    windowHeight: null, // rpx换算px后的窗口高度
+    screenHeight: null, // rpx换算px后的屏幕高度
   }
 })
