db0e1a6aa8e0f823f136a8d0880226c5ca0efc33
Chishin <pro@xxgzs.org>
2019-08-25 09:51:52 +0800

    增加分类名校验与错误提示函数

--- pages/user/class/edit.js
+++ pages/user/class/edit.js
@@ -25,7 +25,7 @@ Page({
   typeChange: function (e) {
     that = this;
     var typeId = e.detail.value;
-    var ClassData = getClassSetData(ClassId, typeId);
+    var ClassData = initClassData(ClassId, typeId);
     console.log(e, ClassData);
     if (ClassData) {
       this.setData(ClassData);
@@ -46,10 +46,15 @@ Page({
    */
   submit_edit: function(res) {
     that = this;
-    var newClassName = res.detail.value.Class_name;
+    var newClassName = res.detail.value.class_name;
     var ClassData = getApp().GetClassId(ClassId);
-    if (ClassData.name != newClassName) {
-      console.log('编辑分类按钮');
+    console.log('编辑分类按钮', ClassData, newClassName);
+    if (ClassData.name == newClassName) {
+      wx.showToast({
+        title: '分类名未修改'
+      });
+    } else {
+
     }
   },
 
@@ -102,7 +107,7 @@ Page({
     that = this;
     uid = wx.getStorageSync('user').uid;
     ClassId = options.id;
-    var ClassData = getClassSetData(ClassId);
+    var ClassData = initClassData(ClassId);
     if (ClassData) {
       that.setData(ClassData);
     } else {
@@ -172,7 +177,7 @@ Page({
 /**
  * 获取Class显示数据
  */
-function getClassSetData(classId, typeId = false) {
+function initClassData(classId, typeId = false) {
   var ClassData = getApp().GetClassId(ClassId);
   var ClassArr = [];
   if(typeId) {
@@ -210,4 +215,234 @@ function getClassSetData(classId, typeId = false) {
   } else {
     return null;
   }
+}
+
+
+/** 错误提示 */
+function showTopTips(text) {
+  that.setData({
+    showTopTips: true,
+    textTopTips: text
+  });
+  setTimeout(function () {
+    that.setData({
+      showTopTips: false,
+      textTopTips: ""
+    });
+  }, 3000);
+}
+
+/**
+ * 校验分类名称
+ */
+function checkClassName(className) {
+  var pattern = /[A-Za-z0-9_\-\u4e00-\u9fa5]+/;
+  return pattern.test(className);
+}
+
+/**
+ * 编辑分类名称
+ */
+function cmdEditClass(classid, classtype, classname) {
+  var editData = {};
+  editData.classid = classid;
+  editData.classname = classname;
+  editData.classtype = classtype;
+  editData.ufid = uid;
+  console.log('表单处理后结果：', editData);
+
+  //数据加密
+  //console.log(JSON.stringify(editData));
+  var strData = Base64.encoder(JSON.stringify(editData));
+
+  //发送数据
+  wx.showLoading({
+    title: '提交中',
+    success: function () {
+      sendClassData(strData, 'edit', function (ret) {
+        wx.hideLoading();
+        if (ret.hasOwnProperty('uid') && (ret.uid > 0)) {
+          if (ret.hasOwnProperty('data') && ret.data[0]) {
+            //编辑分类成功
+            wx.showToast({
+              title: '编辑分类成功',
+            });
+            console.log('编辑分类完成：', ret);
+            //更新分类数据
+            initData(function () {
+              //更新完成
+            });
+          } else {
+            //添加分类失败
+            wx.showModal({
+              title: '编辑分类失败',
+              content: ret.data[1] ? ret.data[1] : '未知错误？',
+              showCancel: false
+            })
+          }
+        } else {
+          //未登陆
+          getApp().Logout(function (path) {
+            wx.redirectTo(path);
+          });
+        }
+      });
+    }
+  });
+}
+
+/**
+ * 分类转移
+ */
+function cmdChangeClass(classid, classtype, classname) {
+  var editData = {};
+  editData.classid = classid;
+  editData.classname = classname;
+  editData.classtype = classtype;
+  editData.ufid = uid;
+
+  //数据加密
+  var strData = Base64.encoder(JSON.stringify(editData));
+
+  //发送数据
+  wx.showLoading({
+    title: '提交中',
+    success: function () {
+      sendClassData(strData, 'change', function (ret) {
+        wx.hideLoading();
+        if (ret.hasOwnProperty('uid') && (ret.uid > 0)) {
+          if (ret.hasOwnProperty('data') && ret.data[0]) {
+            //转移分类成功
+            wx.showToast({
+              title: '转移分类成功',
+            });
+            console.log('转移分类完成：', ret);
+            //更新分类数据
+            initData(function () {
+              //更新完成
+            });
+          } else {
+            //添加分类失败
+            wx.showModal({
+              title: '转移分类失败',
+              content: ret.data[1] ? ret.data[1] : '未知错误？',
+              showCancel: false
+            })
+          }
+        } else {
+          //未登陆
+          getApp().Logout(function (path) {
+            wx.redirectTo(path);
+          });
+        }
+      });
+    }
+  });
+}
+
+/**
+ * 删除分类
+ */
+function cmdDeleteClass(classid) {
+  var delData = {
+    classid: classid
+  };
+
+  //数据加密
+  var strData = Base64.encoder(JSON.stringify(delData));
+
+  //发送数据
+  wx.showLoading({
+    title: '提交中',
+    success: function () {
+      sendClassData(strData, 'del', function (ret) {
+        wx.hideLoading();
+        if (ret.hasOwnProperty('uid') && (ret.uid > 0)) {
+          if (ret.hasOwnProperty('data') && ret.data[0]) {
+            //删除分类成功
+            wx.showToast({
+              title: '删除成功',
+            });
+            console.log('删除分类完成：', ret);
+            //更新分类数据
+            initData(function () {
+              //更新完成
+            });
+          } else {
+            //添加分类失败
+            wx.showModal({
+              title: '删除分类失败',
+              content: ret.data[1] ? ret.data[1] : '未知错误？',
+              showCancel: false
+            })
+          }
+        } else {
+          //未登陆
+          getApp().Logout(function (path) {
+            wx.redirectTo(path);
+          });
+        }
+      });
+    }
+  });
+}
+
+/** 
+ * 发送分类数据(data数组, 回调函数) 
+ */
+function sendClassData(data, type, callback) {
+  var session_id = wx.getStorageSync('PHPSESSID'); //本地取存储的sessionID  
+  if (session_id != "" && session_id != null) {
+    var header = {
+      'content-type': 'application/x-www-form-urlencoded',
+      'Cookie': 'PHPSESSID=' + session_id
+    }
+  } else {
+    var header = {
+      'content-type': 'application/x-www-form-urlencoded'
+    }
+  }
+  wx.request({
+    url: getApp().URL + '/xxjzApp/index.php?s=/Home/Api/aclass',
+    method: 'POST',
+    data: {
+      type: type,
+      data: data
+    },
+    header: header,
+    success: function (res) {
+      console.log('发送分类POST：', res);
+      if (res.hasOwnProperty('data')) {
+        let ret = res['data'];
+        callback(ret);
+      } else {
+        callback({
+          uid: 0,
+          data: err['msg'] + '（请联系管理员）'
+        });
+      }
+    }
+  });
+}
+
+/**
+ * 获取分类数据
+ */
+function getClassData(callback) {
+  getApp().GetClassAllData(parseInt(uid), function (ret, len, data) {
+    if (ret) {
+      callback(data);
+    } else {
+      wx.showModal({
+        title: '重新登录',
+        content: '登录验证已过期，请重新登录。',
+        showCancel: false,
+        success: function () {
+          getApp().Logout(function (path) {
+            wx.redirectTo(path);
+          });
+        }
+      });
+    }
+  });
 }
