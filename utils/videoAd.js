70b4525f7a85cd904545fb2cb2244685eb242e60
Chishin <pro@xxgzs.org>
2020-03-16 15:30:57 +0800

    封装VideoAd模块

--- utils/videoAd.js
+++ utils/videoAd.js
@@ -1,86 +1,110 @@
+export class VideoAd {
 
-var videoAd = null;
-
-/** 加载激励广告组件 */
-function loadAd(callback) {
-  if (wx.createRewardedVideoAd) {
-    videoAd = wx.createRewardedVideoAd({
-      adUnitId: 'adunit-7ccaa4a589fd311a'
-    })
-    videoAd.onLoad(() => {
-      console.log("激励广告加载完成！");
-      callback({
-        status: 'load',
-        message: '广告组件加载完成',
-      });
-      videoAd.offLoad();
-    })
-    videoAd.onError((err) => {
-      console.log('激励广告加载失败:', err);
-      callback({
-        status: 'error',
-        message: err.errMsg,
-      });
-    })
-    videoAd.onClose((res) => {
-      console.log('激励广告关闭事件:', res);
-      if (res.isEnded) {
-        callback({
-          status: 'finsh',
-          message: '广告观看完成',
-        });
-      }
-    })
-  } else {
-    callback({
-      status: 'error',
-      message: '微信版本过低，部分组件无法加载，请升级微信再试。',
-    });
+  constructor() {
+    this.videoAd = null;
   }
-}
 
-/** 显示广告 */
-function showAd() {
-  videoAd.show().catch(() => {
-    // 失败重试
-    videoAd.load()
-      .then(() => videoAd.show())
-      .catch(err => {
-        wx.showModal({
-          title: '无法增加',
-          content: '广告显示失败，请关闭后再试。',
-          showCancel: false,
+  /** 加载激励广告组件 */
+  loadAd = (callback) => {
+    var that = this;
+    if (wx.createRewardedVideoAd) {
+      that.videoAd = wx.createRewardedVideoAd({
+        adUnitId: 'adunit-7ccaa4a589fd311a'
+      })
+      that.videoAd.onLoad(() => {
+        console.log("激励广告加载完成！");
+        callback({
+          status: 'load',
+          message: '广告组件加载完成',
+        });
+        that.videoAd.offLoad();
+      })
+      that.videoAd.onError((err) => {
+        console.log('激励广告加载失败:', err);
+        callback({
+          status: 'error',
+          message: err.errMsg,
         });
       })
-  });
-}
-
-/** 开始执行 */
-function startAd(callback) {
-  if (videoAd) {
-    showAd();
-  } else {
-    wx.showLoading({
-      title: '加载广告',
-      success: function () {
-        loadAd(function (res) {
-          wx.hideLoading();
-          if (res.status == 'load') {
-            showAd();
-          } else if (res.status == 'error') {
-            videoAd = false;
-            wx.showModal({
-              title: '无法增加',
-              content: res.error,
-              showCancel: false,
-            });
-          } else {
-            callback(videoAd);
-          } 
-        });
-      }
+      that.videoAd.onClose((res) => {
+        console.log('激励广告关闭事件:', res);
+        if (res.isEnded) {
+          callback({
+            status: 'finsh',
+            message: '广告观看完成',
+          });
+        }
+      })
+    } else {
+      callback({
+        status: 'error',
+        message: '微信版本过低，部分组件无法加载，请升级微信再试。',
+      });
+    }
+  }
+  
+  /** 显示广告 */
+  showAd = () => {
+    var that = this;
+    that.videoAd.show().catch(() => {
+      // 失败重试
+      that.videoAd.load()
+        .then(() => that.videoAd.show())
+        .catch(err => {
+          wx.showModal({
+            title: '无法增加',
+            content: '广告显示失败，请关闭后再试。',
+            showCancel: false,
+          });
+        })
     });
   }
-}
+  
+  /** 开始执行 */
+  start = (callback) => {
+    var that = this;
+    if (that.videoAd) {
+      that.showAd();
+    } else {
+      wx.showLoading({
+        title: '加载广告',
+        success: function () {
+          that.loadAd(function (res) {
+            wx.hideLoading();
+            if (res.status == 'load') {
+              that.showAd();
+            } else if (res.status == 'error') {
+              that.videoAd = false;
+              wx.showModal({
+                title: '无法增加',
+                content: res.error,
+                showCancel: false,
+              });
+            } else {
+              callback(that.videoAd);
+            } 
+          });
+        }
+      });
+    }
+  }
 
-export const start = startAd;
+  close = () => {
+    if (this.videoAd) {
+      try {
+        this.videoAd.offLoad();
+        this.videoAd.offError();
+        this.videoAd.offClose();
+        if (this.videoAd.destroy) {
+          this.videoAd.destroy(function () {
+            this.videoAd = null;
+          });
+        } else {
+          this.videoAd = null;
+        }
+      } catch (error) {
+        console.log('强制销毁广告实例', error);
+      }
+    }
+  }
+}
