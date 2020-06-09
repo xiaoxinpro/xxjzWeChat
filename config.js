4976fca656995f5f889595f556a8a3344cce6b13
Chishin <pro@xxgzs.org>
2020-06-09 21:43:31 +0800

    创建项目配置文件

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  URL: '请配置服务器地址',
+  AdminUid: 1,
+  MinServerVersion: '2.1.0',
+  Demo:{username:'demo',password:'xxgzs.org'},
+  ImageConfig: {freeCount: 2, maxCount: 9, maxSize: 2*1024*1024, path: '/Uploads', cdn: ''},
+}
