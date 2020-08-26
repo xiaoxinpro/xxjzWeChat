ab5aa96ed3cd5131805ec05d5a4d6ff8be25ad30
Chishin <pro@xxgzs.org>
2020-08-26 21:18:42 +0800

    增加返回按钮事件

--- pages/user/funds/transfer.js
+++ pages/user/funds/transfer.js
@@ -67,6 +67,15 @@ Page({
       dateStr: "" + year + "年" + month + "月" + day + "日"
     })
   },
+  
+  /**
+   * 返回按钮
+   */
+  bindBack: function() {
+    wx.navigateBack({
+      delta: 1
+    });
+  },
 
   /**
    * 生命周期函数--监听页面加载
